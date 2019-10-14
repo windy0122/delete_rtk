bea3828c11df27ebce21b346f53977d53db35155
windy0122 <575093904@qq.com>
2019-10-14 15:24:10 +0800

    随便改改

--- delete_cfg_uat.py
+++ delete_cfg_uat.py
@@ -7,6 +7,7 @@ class DeleteRtkTest(object):
     def __init__(self):
         self.test_data = ReadIni(node='Uat')
 
+        # 连接redis uat环境
         self.host_uat = self.test_data.get_value('host_uat_redis')
         self.port_uat = self.test_data.get_value('port_uat_redis')
         self.password_uat = self.test_data.get_value('password_uat_redis')
@@ -14,6 +15,7 @@ class DeleteRtkTest(object):
 
         # self.r_test = DeleteRtkTest.connect_redis(self)
 
+        # 连接uat数据库
         self.connection_test = MySQLdb.connect(
             host=self.test_data.get_value('host_uat'),
             user=self.test_data.get_value('user_uat'),
@@ -28,15 +30,17 @@ class DeleteRtkTest(object):
     def sql_handle(self, member_id):
         # c = DeleteRtkTest.connect_data(self)
 
+        # 删除rtk数据所需的sql语句
         sql = 'select * from tb_member_customer where id=' + member_id
         sql1 = 'delete from tb_member_customer_ascription where Member_Customer_Id=' + member_id
         sql2 = 'delete from tb_member_customer_extend_info where Member_Customer_Id=' + member_id
         sql3 = 'delete from tb_member_customer where ID=' + member_id
 
-        rows_aff = self.c.execute(sql)
+        # 通过sql获取顾客的open_id
+        self.c.execute(sql)
         open_id = self.c.fetchall()[0][4]
 
-        # return open_id
+        # 通过open_id 获取redis中的key
         login_key = self.r_test.keys('rts:LoginToken.' + open_id)
         print(login_key)
         login_get_key = self.r_test.get('rts:LoginToken.' + open_id)
@@ -46,6 +50,7 @@ class DeleteRtkTest(object):
         login_get_key1 = self.r_test.keys('rts:LoginToken.' + eval(login_get_key))
         print(login_get_key1)
 
+        # noinspection PyBroadException
         try:
             # 执行sql语句
             self.c.execute(sql1)
@@ -59,7 +64,7 @@ class DeleteRtkTest(object):
 
             self.r_test.delete(login_key[0])
 
-        except:
+        except Exception:
             # 如果失败会回滚到上一个版本
             print('执行失败')
             # connection.rollback()
